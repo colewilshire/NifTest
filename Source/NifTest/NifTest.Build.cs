8dfd0e1f9489607cc4524de14e1a70bee8943471
COLE-DESKTOP\colew <cole.wilshire@outlook.com>
2025-07-27 14:53:56 -0500

    Removed Nifly and Example plugins.

--- Source/NifTest/NifTest.Build.cs
+++ Source/NifTest/NifTest.Build.cs
@@ -8,7 +8,7 @@ public class NifTest : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Nifly", "Niflib" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Niflib" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
--- Source/NifTest/NifTestActor.cpp
+++ Source/NifTest/NifTestActor.cpp
@@ -1,9 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "NifTestActor.h"
-//#include "NifFile.hpp"
-//#include "Miniball.hpp"
 #include "niflib.h"
 
 // Sets default values
@@ -43,35 +40,3 @@ void ANifTestActor::NiflibTest()
         UE_LOG(LogTemp, Error, TEXT("Failed to open NIF file: %s"), *NifPath);
     }
 }
-
-// void ANifTestActor::LoadFile()
-// {
-// 	FString AbsoluteFilePath = TEXT("E:/Program Files (x86)/Steam/steamapps/common/The Guild 2 Renaissance/Objects/Animals/bull.nif");
-// 	TArray<uint8> BinaryData;
-
-// 	int64 UnixTime = FDateTime::UtcNow().ToUnixTimestamp();
-// 	UE_LOG(LogTemp, Warning, TEXT("Current Unix time: %lld"), UnixTime);
-
-// 	if (FFileHelper::LoadFileToArray(BinaryData, *AbsoluteFilePath))
-// 	{
-// 		UE_LOG(LogTemp, Warning, TEXT("Loaded file successfully. File size: %d bytes"), BinaryData.Num());
-// 	}
-// 	else
-// 	{
-// 		UE_LOG(LogTemp, Error, TEXT("Failed to load file: %s"), *AbsoluteFilePath);
-// 	}
-
-// 	float TestValue = 2.0f;
-//     float Squared = Miniball::mb_sqr<float>(TestValue);
-//     UE_LOG(LogTemp, Warning, TEXT("Nifly Test: The square of %f is %f"), TestValue, Squared);
-
-// 	std::filesystem::path FilePathA("E:\\Program Files (x86)\\Steam\\steamapps\\common\\The Guild 2 Renaissance\\Objects\\Animals\\bull.nif");
-// 	nifly::NifFile nifA;
-// 	int a = nifA.Load(FilePathA);
-// 	UE_LOG(LogTemp, Warning, TEXT("Output A is %d"), a);
-
-// 	std::filesystem::path FilePathB("E:\\Test");
-// 	nifly::NifFile nifB;
-// 	int b = nifB.Load(FilePathB);
-// 	UE_LOG(LogTemp, Warning, TEXT("Output B is %d"), b);
-// }
