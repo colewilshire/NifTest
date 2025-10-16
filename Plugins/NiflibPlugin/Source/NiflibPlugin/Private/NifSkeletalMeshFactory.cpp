4fd3e850821ba546901c9e9e5c1ef2fac1b17867
COLE-DESKTOP\colew <cole.wilshire@outlook.com>
2025-10-16 04:49:54 -0500

    Made GetRawBoneInfluences, GetWedges, and GetMeshFaces functions, which are more directly Unreal-style ways to unpack Nif data. GetMeshFaces is currently WIP.

--- Plugins/NiflibPlugin/Source/NiflibPlugin/Private/NifSkeletalMeshFactory.cpp
+++ Plugins/NiflibPlugin/Source/NiflibPlugin/Private/NifSkeletalMeshFactory.cpp
@@ -9,14 +9,150 @@
 #include "ReferenceSkeleton.h"
 #include "AssetToolsModule.h"
 #include "AssetRegistry/AssetRegistryModule.h"
+#include <Rendering/SkeletalMeshLODImporterData.h>
 
 // Niflib
 #include <niflib.h>
 #include <obj/NiLODNode.h>
+#include <obj/NiTriShapeData.h>
 #include <obj/NiSkinInstance.h>
+#include <obj/NiSkinData.h>
 #include <obj/NiMultiTargetTransformController.h>
+#include <obj/NiMaterialProperty.h>
 
 using namespace Niflib;
+
+static TArray<SkeletalMeshImportData::FRawBoneInfluence> GetRawBoneInfluences(const NiSkinInstanceRef& SkinInstance)
+{
+	const std::vector<NiNodeRef>& Bones = SkinInstance->GetBones();
+	const NiSkinDataRef& SkinData = SkinInstance->GetSkinData();
+	TArray<SkeletalMeshImportData::FRawBoneInfluence> RawBoneInfluences;
+
+	for (int BoneIndex = 0; BoneIndex < Bones.size(); BoneIndex++)	// Bone indices will be off by 2 from the reference skeleton, since its only counting children of the skin. Adjust.
+	{
+		const std::vector<SkinWeight>& SkinWeights = SkinData->GetBoneWeights(BoneIndex);
+		for (SkinWeight SkinWeight : SkinWeights)
+		{
+			SkeletalMeshImportData::FRawBoneInfluence RawBoneInfluence{ SkinWeight.index, BoneIndex, SkinWeight.weight };
+			RawBoneInfluences.Add(RawBoneInfluence);
+		}
+	}
+
+	return RawBoneInfluences;
+}
+
+static TArray<SkeletalMeshImportData::FMeshWedge> GetMeshWedges(const NiGeometryDataRef& GeometryData)
+{
+	const std::vector<Vector3>& Vertices = GeometryData->GetVertices();
+	const std::vector<Color4>& Colors = GeometryData->GetColors();
+	TArray<SkeletalMeshImportData::FMeshWedge> MeshWedges;
+
+	for (int VertexIndex = 0; VertexIndex < Vertices.size(); VertexIndex++)
+	{
+		SkeletalMeshImportData::FMeshWedge MeshWedge = {};
+		MeshWedge.iVertex = VertexIndex;
+
+		for (int i = 0; i < GeometryData->GetUVSetCount(); i++)
+		{
+			const std::vector<TexCoord>& UVSet = GeometryData->GetUVSet(i);
+			const FVector2f UV = { UVSet[VertexIndex].u, UVSet[VertexIndex].v};
+			MeshWedge.UVs[i] = UV;
+		}
+
+		if (!Colors.empty())
+		{
+			FColor Color = { Colors[VertexIndex].b, Colors[VertexIndex].g, Colors[VertexIndex].r, Colors[VertexIndex].a };
+			MeshWedge.Color = Color;
+		}
+
+		MeshWedges.Add(MeshWedge);
+	}
+
+	return MeshWedges;
+}
+
+static TArray<SkeletalMeshImportData::FMeshFace> GetMeshFaces(const NiTriShapeRef& TriShape)
+{
+	const NiTriShapeDataRef& TriShapeData = DynamicCast<NiTriShapeData>(TriShape->GetData());
+	const std::vector<Triangle>& Triangles = TriShapeData->GetTriangles();
+	const std::vector<Vector3>& Tangents = TriShapeData->GetTangents();
+	const std::vector<Vector3>& Bitangents = TriShapeData->GetBitangents();
+	const std::vector<Vector3>& Normals = TriShapeData->GetNormals();
+	TArray<SkeletalMeshImportData::FMeshFace> MeshFaces;
+
+	for (Triangle Triangle : Triangles)
+	{
+		SkeletalMeshImportData::FMeshFace MeshFace = {};
+		const uint32 VertexIndices[3] = { Triangle.v1, Triangle.v2, Triangle.v3 };
+
+		for (int i = 0; i < std::size(VertexIndices); i++)
+		{
+			const FVector3f& Tangent = { Tangents[VertexIndices[i]].x, Tangents[VertexIndices[i]].y, Tangents[VertexIndices[i]].z };
+			const FVector3f& Bitangent = { Bitangents[VertexIndices[i]].x, Bitangents[VertexIndices[i]].y, Bitangents[VertexIndices[i]].z };
+			const FVector3f& Normal = { Normals[VertexIndices[i]].x, Normals[VertexIndices[i]].y, Normals[VertexIndices[i]].z };
+
+			MeshFace.iWedge[i] = VertexIndices[i];
+			MeshFace.TangentX[i] = Tangent;
+			MeshFace.TangentY[i] = Bitangent;
+			MeshFace.TangentZ[i] = Normal;
+		}
+
+		MeshFaces.Add(MeshFace);
+	}
+	const NiPropertyRef& Property = TriShape->GetPropertyByType(NiMaterialProperty::TYPE);
+	const auto& MaterialProperty = DynamicCast<NiMaterialProperty>(Property);
+
+	return MeshFaces;
+}
+
+static SkeletalMeshImportData::FMeshFace GetMeshFace()
+{
+	SkeletalMeshImportData::FMeshFace MeshFace;
+	// uint32 iWedge[3];    // The 3 wedge indices making up this face
+	// uint16 MeshMaterialIndex; // Which material slot this face belongs to
+	// uint32 SmoothingGroups;   // Smoothing group for shading
+	// FVector3f TangentX[3];    // Per-corner tangent (optional at import)
+	// FVector3f TangentY[3];    // Per-corner bitangent
+	// FVector3f TangentZ[3];    // Per-corner normal
+
+	return MeshFace;
+}
+
+static void Test(const FString& Filename)
+{
+	/*TriShape->GetSkinInstance()->GetSkinData()->GetBoneWeights(0)[0].
+	DynamicCast<NiTriShapeData>(TriShape->GetData())->GetTriangles()[0].
+	TriShape->GetData()->get
+	TriShape->GetSkinInstance()->GetSkinData()->GetBoneWeights(0)[0].
+	TriShape->GetSkinInstance()->GetBones()*/
+
+	std::vector<NiObjectRef> NifList = ReadNifList(TCHAR_TO_UTF8(*Filename));
+
+	for (const NiObjectRef& Object : NifList)
+	{
+		const NiTriShapeRef& TriShape = DynamicCast<NiTriShape>(Object);
+		if (TriShape)
+		{
+			const std::vector<NiNodeRef>& Bones = TriShape->GetSkinInstance()->GetBones();
+			const NiSkinDataRef& SkinData = TriShape->GetSkinInstance()->GetSkinData();
+			for (int i = 0; i < Bones.size(); i++)
+			{
+				UE_LOG(LogTemp, Log, TEXT("[NIF] %d"), i);
+				const std::vector<SkinWeight>& SkinWeights = SkinData->GetBoneWeights(i);
+				for (SkinWeight SkinWeight : SkinWeights)
+				{
+					UE_LOG(LogTemp, Log, TEXT("	[NIF] %hu: %.2f"), SkinWeight.index, SkinWeight.weight);
+				}
+			}
+			break;
+
+			TArray<int32> BoneIndices;
+			BoneIndices.SetNum(TriShape->GetData()->GetVertexCount());
+			TArray<float> Weights;
+			Weights.SetNum(TriShape->GetData()->GetVertexCount());
+		}
+	}
+}
 
 // Create a unique package under the same folder as the selected destination
 static UPackage* MakeAssetPackage(const FString& BasePath, const FString& AssetName, FString& OutObjectName)
@@ -74,6 +210,7 @@ UObject* UNifSkeletalMeshFactory::FactoryCreateFile(
 
 	// Parse NIF for mesh data
 	UNifSkeletalMeshFactory::FNifReferenceSkeleton NifReferenceSkeleton = ParseNif(Filename);
+	Test(Filename);
 
 	// Create packages/assets
 	const FString BasePath = InParent->GetOutermost()->GetName();
@@ -182,7 +319,7 @@ UNifSkeletalMeshFactory::FNifReferenceSkeleton UNifSkeletalMeshFactory::ParseNif
 		}
 	}
 
-	for (const auto& Pair : Map)
+	/*for (const auto& Pair : Map)
 	{
 		FString s = UTF8_TO_TCHAR(Pair.first->GetIDString().c_str());
 		UE_LOG(LogTemp, Log, TEXT("[NIF] IDString: %s"), *s);
@@ -192,7 +329,7 @@ UNifSkeletalMeshFactory::FNifReferenceSkeleton UNifSkeletalMeshFactory::ParseNif
 			FString t = UTF8_TO_TCHAR(Child->GetIDString().c_str());
 			UE_LOG(LogTemp, Log, TEXT("	[NIF] IDString: %s"), *t);
 		}
-	}
+	}*/
 
 	return Skeleton;
 }
